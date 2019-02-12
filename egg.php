fb6f69b949cffd29e550a35d64e7372fef6db14d
Vasilii B. Shpilchin <order@vasya.pro>
2019-02-12 01:14:45 -0500

    Create egg.php

---
+++ egg.php
@@ -0,0 +1,54 @@
+<?php
+
+class Egg {
+
+    private $instance_id;
+
+    function __construct(\Closure $callback) {
+        ($this->_($callback))();
+    }
+
+    private function _(\Closure $callback) {
+        return Closure::bind($callback, $this, 'Egg');
+    }
+
+}
+
+new Egg(function() {
+
+    $router = $this->_(function($path) {
+        if (empty($path)) {
+            $path = '/';
+        }
+        $this->route = [
+            'path' => $path,
+            'controller' => 'indexController',
+            'action' => 'indexAction',
+            'params' => []
+        ];
+        $parts = explode('/', $path);
+        if (count($parts) && strlen($parts[0])) {
+            $this->route['controller'] = array_shift($parts) . 'Controller';
+        }
+        if (count($parts)) {
+            $this->route['action'] = array_shift($parts) . 'Action';
+        }
+        $this->route['params'] = $parts;
+        return $this->route['controller'];
+    });
+
+    $indexController = $this->_(function() {
+
+        $indexAction = $this->_(function($params) {
+            echo 'This is Egg';
+        });
+
+        $helloAction = $this->_(function($params) {
+            echo 'Hello, ', $params[0], '!';
+        });
+
+        ${$this->route['action']}($this->route['params']);
+    });
+
+    ${$router($_GET['path'])}();
+});
